6de0124df2d580058f78f850a22787914174bf5c
Kevin Glowacz <kevin@glowacz.info>
2010-12-21 21:15:25 -0600

    improved rake and cap bash completion

--- rake_cap_bash_autocomplete.sh
+++ rake_cap_bash_autocomplete.sh
@@ -1,14 +1,36 @@
+# from https://github.com/jpalardy/dotfiles/blob/master/bash/completion
+
 export COMP_WORDBREAKS=${COMP_WORDBREAKS/\:/}
 
-_rakecomplete() {
-  COMPREPLY=($(compgen -W "`rake -s -T 2>/dev/null | awk '{{print $2}}'`" -- ${COMP_WORDS[COMP_CWORD]}))
-  return 0
+_check_rakefile() {
+  if [ ! -e Rakefile ]; then
+    return
+  fi
+
+  local cache_file=".cache_rake_t"
+
+  if [ ! -e "$cache_file" ]; then
+    rake -T | awk '/^rake / {print $2}' > $cache_file
+  fi
+
+  local tasks=$(cat $cache_file)
+  COMPREPLY=( $(compgen -W "${tasks}" -- $2) )
 }
 
-_capcomplete() {
-  COMPREPLY=($(compgen -W "`cap  -T  2>/dev/null| awk '{{ if ( $3 ~ /\#/ ) print $2}}'`" -- ${COMP_WORDS[COMP_CWORD]}))
-  return 0
+_check_capfile() {
+  if [ ! -e Capfile ]; then
+    return
+  fi
+
+  local cache_file=".cache_cap_t"
+
+  if [ ! -e "$cache_file" ]; then
+    cap -T | awk '/^cap / {print $2}' > $cache_file
+  fi
+
+  local tasks=$(cat $cache_file)
+  COMPREPLY=( $(compgen -W "${tasks}" -- $2) )
 }
 
-complete -o default -o nospace -F _rakecomplete rake
-complete -o default -o nospace -F _capcomplete cap
+complete -F _check_rakefile -o default rake
+complete -F _check_capfile -o default cap
