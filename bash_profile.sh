46b33ecbc00ecb2d0772453a6b5fe533c270a0b7
Kevin Glowacz <kevin@glowacz.info>
2021-03-30 16:47:24 -0500

    Update bash_profile for newer bash-completion script

--- bash_profile.sh
+++ bash_profile.sh
@@ -39,6 +39,8 @@ if (which brew &> /dev/null) && [ -f `brew --prefix`/etc/bash_completion ]; then
   . `brew --prefix`/etc/bash_completion
 fi
 
+[[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
+
 if [ -f ~/.cap_bash_autocomplete ]; then
   source ~/.cap_bash_autocomplete
 fi
@@ -49,36 +51,9 @@ fi
 
 [ -f "$HOME/.secrets" ] && source "$HOME/.secrets"
 
-# Author.: Ole J
-# Date...: 23.03.2008
-# License: Whatever
+_completion_loader git
+__git_complete gco _git_checkout
 
-# Wraps a completion function
-# make-completion-wrapper <actual completion function> <name of new func.>
-#                         <command name> <list supplied arguments>
-# eg.
-#   alias agi='apt-get install'
-#   make-completion-wrapper _apt_get _apt_get_install apt-get install
-# defines a function called _apt_get_install (that's $2) that will complete
-# the 'agi' alias. (complete -F _apt_get_install agi)
-#
-function make-completion-wrapper () {
-  local function_name="$2"
-  local arg_count=$(($#-3))
-  local comp_function_name="$1"
-  shift 2
-  local function="
-function $function_name {
-  ((COMP_CWORD+=$arg_count))
-  COMP_WORDS=( "$@" \${COMP_WORDS[@]:1} )
-  "$comp_function_name"
-  return 0
-}"
-  eval "$function"
-}
-
-make-completion-wrapper _git _gco git checkout
-complete -o default -o nospace -F _gco gco
 
 flushdns()
 {
